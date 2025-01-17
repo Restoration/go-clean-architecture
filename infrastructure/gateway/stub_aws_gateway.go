c18199ad25a8569059dcd1e0eed1f59b6dd5f8bf
RyotArch <r.yamamoto.system@gmail.com>
2025-01-17 20:29:10 +0900

    Add span

--- infrastructure/gateway/stub_aws_gateway.go
+++ infrastructure/gateway/stub_aws_gateway.go
@@ -1,5 +1,12 @@
 package gateway
 
+import (
+	"context"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+)
+
 type StubAWSGateway struct{}
 
 func NewStubAWSGateway() *StubAWSGateway {
@@ -7,5 +14,16 @@ func NewStubAWSGateway() *StubAWSGateway {
 }
 
 func (repo *StubAWSGateway) CreatePreSignedURL(bucket string, storageKey int) (url *string, err error) {
+	tracer := otel.Tracer("go-clean-app.gateway")
+	_, span := tracer.Start(context.Background(), "StubAWSGateway.CreatePreSignedURL")
+	defer span.End()
+	// 何らかの処理
+	// 完了後Spanに記録
+	span.SetAttributes(
+		attribute.String("aws.service", "s3"),
+		attribute.String("aws.operation", "Presigned URL"),
+		attribute.String("aws.bucket", bucket),
+		attribute.Int("aws.storageKey", storageKey),
+	)
 	return nil, nil
 }
--- infrastructure/middleware/opentelemetry.go
+++ infrastructure/middleware/opentelemetry.go
@@ -11,17 +11,26 @@ func OpenTelemetryMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx := c.Request.Context()
 		tracer := otel.Tracer("go-clean-app")
+		// エンドポイントパス
+		path := c.FullPath()
+		if path == "" {
+			path = "unknown" // パスが取得できない場合のデフォルト値
+		}
 		// トレースの開始
 		ctx, span := tracer.Start(ctx, "http.request",
 			trace.WithAttributes(
 				attribute.String("http.method", c.Request.Method),
 				attribute.String("http.url", c.Request.URL.String()),
 				attribute.String("http.client_ip", c.ClientIP()),
-				attribute.Int("http.status_code", c.Writer.Status()),
+				attribute.String("http.path", path),
 			),
 		)
 		defer span.End()
 		c.Request = c.Request.WithContext(ctx)
 		c.Next()
+		// ステータスコードの記録は c.Next() 実行後
+		span.SetAttributes(
+			attribute.Int("http.status_code", c.Writer.Status()),
+		)
 	}
 }
